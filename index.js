1fec1bf993bd6edf4efaef3bb5e132f2a22d3d3d
Marvin Diaz <md1685268@gmail.com>
2025-02-04 22:48:28 -0800

    #9 bugs found and fixed

--- index.js
+++ index.js
@@ -27,7 +27,12 @@ function checkGuess() {
   const guess = parseInt(guessInput.value, 10);
   attempts = attempts + 1;
 
-  hideAllMessages();
+  //fix bug ninth per instructions need to define function
+  function hideAllMessages() {
+    for (const message of messages) {
+      message.stye.display = 'none';
+    }
+  }
 
   if (guess === targetNumber) {
     numberOfGuessesMessage.style.display = '';
@@ -38,12 +43,12 @@ function checkGuess() {
     submitButton.disabled = true;
     guessInput.disabled = true;
   }
-
+// Fixed eight bug,tooLowMessage repited, should be tooHighMessage
   if (guess !== targetNumber) {
     if (guess < targetNumber) {
       tooLowMessage.style.display = '';
     } else {
-      tooLowMessage.style.display = '';
+      tooHighMessage.style.display = '';
     }
 
     const remainingAttempts = maxNumberOfAttempts - attempts;
@@ -52,7 +57,8 @@ function checkGuess() {
     numberOfGuessesMessage.innerHTML = `You guessed ${guess}. <br> ${remainingAttempts} guesses remaining`;
   }
 
-  if (attempts ==== maxNumberOfAttempts) {
+  //Fixed first bug, too many equal signs
+  if (attempts === maxNumberOfAttempts) {
     submitButton.disabled = true;
     guessInput.disabled = true;
   }
@@ -61,23 +67,27 @@ function checkGuess() {
 
   resetButton.style.display = '';
 }
-
+//Fixed seventh bug the sign [] dosen't apply with elementIndex
+//Fixed sixth bug element index
+//Fixed fifth bug function not defined
 function hideAllMessages() {
-  for (let elementIndex = 0; elementIndex <= messages.length; elementIndex++) {
+  let elementIndex = 0; elementIndex <= messages.length; elementIndex++ 
     messages[elementIndex].style.display = 'none';
-  }
 }
 
-funtion setup() {
+//Fixed second bug word function misspelled 
+function setup() {
   // Get random number
   targetNumber = getRandomNumber(1, 100);
   console.log(`target number: ${targetNumber}`);
 
+//Fixed third bug, number of attempts should be 5 or less
   // Reset number of attempts
-  maxNumberOfAttempts = 0;
+  const maxNumberOfAttempts = 5;
 
+//Fixed fourth bug changed disabeld to disabled on sub button
   // Enable the input and submit button
-  submitButton.disabeld = false;
+  submitButton.disabled = false;
   guessInput.disabled = false;
 
   hideAllMessages();
